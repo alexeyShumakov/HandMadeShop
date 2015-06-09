81b888d70fd5a29239f99158c2f08014eafc6c8a
alexeyShumakov <bobshaftoe@yandex.ru>
2015-06-09 13:21:05 +0500

    carousel set up

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   devise_for :users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  root to: 'visitors#index'
+  root to: 'categories#index'
 end
