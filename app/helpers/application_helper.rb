572fde8aed3bbb0189eae09f4812e510b40580bd
alexeyShumakov <bobshaftoe@yandex.ru>
2015-06-14 14:31:12 +0500

    title you

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,7 @@ module ApplicationHelper
 	def custom_currency(price)
 		number_to_currency(price, unit: "", separator: ",", delimiter: " ")
 	end
+	def prefix_title
+		'HandMade, Хэнд Мэйд в Челябинскe.'
+	end
 end
