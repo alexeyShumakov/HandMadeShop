38cba8fb3396db33344ace0d3eea28e96afa93b0
alexeyShumakov <bobshaftoe@yandex.ru>
2015-07-22 06:41:19 +0500

    First update from Vim! awesome

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,4 @@
+# Product controller
 class ProductsController < InheritedResources::Base
   before_action :set_categories, only: :search
   before_action :set_keyword, only: :search
@@ -8,15 +9,19 @@ class ProductsController < InheritedResources::Base
       @products = Product.search_by_title(@keyword).page(params[:page])
     end
   end
-  private
-    def set_keyword
-      @keyword = params[:search]
-    end
-    def set_categories
-      @categories = Category.all
-    end
-    def product_params
-      params.require(:product).permit(:title, :color, :description, :category_id, :price)
-    end
-end
 
+  private
+
+  def set_keyword
+    @keyword = params[:search]
+  end
+
+  def set_categories
+    @categories = Category.all
+  end
+
+  def product_params
+    params.require(:product)
+      .permit(:title, :color, :description, :category_id, :price)
+  end
+end
